44fc32eee47a0781c11d212f71e118f55f98bf0b
Coded mind <shahzadekhan296@gmail.com>
2026-07-13 16:18:52 +0530

    Changes the app.ts and update it

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -21,7 +21,15 @@ app.use(cors({
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ limit: "2mb", extended: true }));
 app.use(express.static("public"));
-app.use(sessionMiddleware)
+
+
+app.use((req, res, next) => {
+  if (req.path.startsWith("/api/inngest")) {
+    return next();
+  }
+
+  sessionMiddleware(req, res, next);
+});
 
 app.all('/api/auth/*any', toNodeHandler(auth));
 
