2dfb2a13e299461b3b965065b2b7e521e211f3d0
Coded_mind__ <shahzadekhan296@gmail.com>
2026-07-13 12:56:36 +0530

    added the vercel.json in server

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -12,7 +12,7 @@ ConnectDb()
         startGraphqlServer()
 
         app.listen(process.env.PORT || 5000, () => {
-            console.log(`✅ Server is running successfully on port: ${process.env.PORT || 8000}`)
+            console.log(`✅ Server is running successfully on port: ${process.env.PORT || 5000}`)
         })
 
     }).catch((err) => {
